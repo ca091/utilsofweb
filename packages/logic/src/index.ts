75c3556237f73b48eaa486679021d781613e260d
erluzi <747877078@qq.com>
2021-04-16 15:09:31 +0800

    test: sha256 with rsa

--- packages/logic/src/index.ts
+++ packages/logic/src/index.ts
@@ -1,11 +1,13 @@
 // export {Countdown} from './Countdown'
 
-export {DBManager} from './DBManager'
-export {PageManager} from './PageManager'
-export {generateFetch} from './http'
-export {lazyImg} from './lazyImg'
-export {screenfull} from './screenfull'
+// export {DBManager} from './DBManager'
+// export {PageManager} from './PageManager'
+// export {generateFetch} from './http'
+// export {lazyImg} from './lazyImg'
+// export {screenfull} from './screenfull'
 
 
 // 应用打包
 // export {generateFetch} from './http'
+
+export {screenfull} from './screenfull'
