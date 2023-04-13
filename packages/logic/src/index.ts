e2f913ddb38facb4d3acda67698075afdbb8617a
erluzi <747877078@qq.com>
2023-04-13 15:09:13 +0800

    fix：code

--- packages/logic/src/index.ts
+++ packages/logic/src/index.ts
@@ -1,13 +1,6 @@
 // export {Countdown} from './Countdown'
-
-// export {DBManager} from './DBManager'
-// export {PageManager} from './PageManager'
-// export {generateFetch} from './http'
-// export {lazyImg} from './lazyImg'
-// export {screenfull} from './screenfull'
-
-
-// 应用打包
-// export {generateFetch} from './http'
-
+export {DBManager} from './DBManager'
+export {PageManager} from './PageManager'
+export {generateFetch} from './http'
+export {lazyImg} from './lazyImg'
 export {fullscreen, fullscreenIos} from './screenfull'
